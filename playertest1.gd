8df64670dfde4c4c5a29afa2d75627b1540bc877
Asher Gomez <gomezasheru@gmail.com>
2021-07-11 00:01:21 -0500

    Edits

--- playertest1.gd
+++ playertest1.gd
@@ -1,34 +1,30 @@
 extends KinematicBody2D
 
-
-
 var moveSpeed = 120
 var health = 100
 var velocity = Vector2.ZERO
 
 onready var healthBar = get_parent().get_node("healthBar")
-func _ready():
-	pass
+
+func _physics_process(delta):
+	get_input()
 	
-#heres where i will put movement shit plus inputs
+	healthBar.value = health
+	if (healthBar.value <= 0):
+		self.queue_free()
+	
+	velocity = velocity.normalized() * moveSpeed
+	velocity = move_and_slide(velocity, Vector2.UP)
 
 func get_input():
-	
 	velocity.x = 0
 	if Input.is_action_pressed("p1Left"):
-		velocity.x -= moveSpeed
-	if Input.is_action_pressed("p1Right"):
-		velocity.x += moveSpeed/0.7
-	if Input.is_action_pressed("p1Down"):
+		velocity.x += (moveSpeed/2) * -1
+	elif Input.is_action_pressed("p1Right"):
+		velocity.x += moveSpeed
+	elif Input.is_action_pressed("p1Down"):
 		pass
-	if Input.is_action_pressed("p1Up"):
+	elif Input.is_action_pressed("p1Up"):
 		pass
 	if Input.is_action_just_pressed("ui_focus_next"):
-		health -= 10
-		
-		velocity = velocity.normalized() * moveSpeed
-		
-func _process(delta):
-	healthBar.value = health
-	get_input()
-	velocity = move_and_slide(velocity,Vector2.UP)
+		health -= 5
