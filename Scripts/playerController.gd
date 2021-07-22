9d3de22e72fb473fff4eda9db2c02fd2368e4e54
pitzamachine <1mrh12705@gmail.com>
2021-07-22 12:22:58 -0500

    made attacks kind of work just need to find if the other player is blocking

--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -20,8 +20,7 @@ var isDashing = false
 var inputTimeout = 0.5
 var canAttack = true
 var attackDir = "right"
-var p1block = "not"
-var p2block = "not"
+var block = "not"
 var hitType = "nothing"
 var recovery = false
 var hitStun = 0
@@ -41,8 +40,7 @@ onready var healthBar = get_parent().get_node("p" + str(playerNumber) + "Health"
 onready var dashAudioCue = $dashAudioCue
 
 func _physics_process(_delta):
-	print(p2block)
-	print(p1block)
+	
 	if playerNumber == 1:
 		enemy = get_tree().get_root().get_node("World/Viewport/Player2")
 	else:
@@ -70,30 +68,30 @@ func _physics_process(_delta):
 	velocity = move_and_slide(velocity, Vector2.DOWN)
 
 func movement_input():
-	p1block = "not"
-	p2block = "not"
+	block = "not"
+	
 
 	if Input.is_action_pressed("p" + str(playerNumber) + "Left"):
 #		print("Left!")
 		
 		if (playerNumber == 2):
 			position.x -= moveSpeed 
-			p2block = "not"
+			block = "not"
 			attackDir = "Left"
 		else:
 			position.x -= backSpeed * 0.3
-			p1block = "Standing"
+			block = "Standing"
 			attackDir = "Left"
 	elif Input.is_action_pressed("p" + str(playerNumber) + "Right"):
 #		print("Right!")
 		
 		if (playerNumber == 1):
 			position.x += moveSpeed
-			p1block = "not"
+			block = "not"
 			attackDir = "Right"
 		else:
 			position.x += backSpeed * 0.3
-			p2block = "Standing"
+			block = "Standing"
 			attackDir = "Right"
 	else:
 		pass
@@ -113,57 +111,6 @@ func animation_handler():
 	else:
 		$Sprite.play("idle")
 
-#func dash_input():
-#	if Input.is_action_just_pressed("p" + str(playerNumber) + "Right") :
-#		rightDash += 1
-#		yield(get_tree().create_timer(inputTimeout), "timeout")
-#	elif Input.is_action_just_pressed("p" + str(playerNumber) + "Left"):
-#		leftDash += 1
-#		yield(get_tree().create_timer(inputTimeout), "timeout")
-#	if rightDash >= 2 and canDash:
-#		canDash = false
-#		isDashing = true
-#		dashRight(position.x)
-#		yield(get_tree().create_timer(dashCooldown), "timeout")
-#		canDash = true
-#	if leftDash >= 2 and canDash:
-#		canDash = false
-#		isDashing = true
-#		dashLeft(position.x)
-#		yield(get_tree().create_timer(dashCooldown), "timeout")
-#		canDash = true
-#
-#func dashRight(currentPos):
-#	print("Dash!")
-#	if (playerNumber == 2):
-#		while (currentPos != currentPos + backDash):
-#			position.x = lerp(currentPos, currentPos + backDash, 0.25)
-#			backDash -= backDash/0.25
-#		isDashing = false
-#		backDash = backDashDefault
-#	else:
-#		while (currentPos != currentPos + forwardsDash):
-#			position.x = lerp(currentPos, currentPos + forwardsDash, 0.25)
-#			forwardsDash -= forwardsDash/0.25
-#		isDashing = false
-#		forwardsDash = forwardsDashDefault
-#	rightDash = 0
-#
-#func dashLeft(currentPos):
-#	print("Dash!")
-#	if (playerNumber == 2):
-#		while (currentPos != currentPos - forwardsDash):
-#			position.x = lerp(currentPos, currentPos - forwardsDash, 0.25)
-#			forwardsDash -= forwardsDash/0.25
-#		isDashing = false
-#		forwardsDash = forwardsDashDefault
-#	else:
-#		while (currentPos != currentPos - backDash):
-#			position.x = lerp(currentPos, currentPos - backDash, 0.25)
-#			backDash -= backDash/0.25
-#		isDashing = false
-#		backDash = backDashDefault
-#	leftDash = 0
 
 #stats
 #damage: down a = 20, forward a = 25, back a = 15 + 15, up a = 40
@@ -180,6 +127,7 @@ func attackCheck():
 		yield(get_tree().create_timer(0.1833333), "timeout")
 		print("Hitboxes.")
 		velocity.x += 20
+		attack("punch")
 		yield(get_tree().create_timer(0.008333), "timeout")
 		print("Recovering...")
 		recovery = true
@@ -192,7 +140,7 @@ func attackCheck():
 		canAttack = true
 		print("Can attack now.")
 	if Input.is_action_just_pressed("p" + str(playerNumber) + "A") and canAttack and attackDir == "Left":
-		print("Down Attack!")
+		print("Left Attack!")
 		canAttack = false
 		backSpeed = 0
 		moveSpeed = 0
@@ -214,34 +162,44 @@ func attack(type):
 	if playerNumber == 1:
 		match type:
 			"lowkick":
+				block = "Standing"
 				if (abs(enemy.position.x - position.x) <= 48):
 					print("Attacking Player " + str(enemy.playerNumber) + ".")
-					if p2block == "Down":
-						enemy.health -= 2
+					if block == "Standing":
+						enemy.health -= 4
 						enemy.hitStun += 11
 					else:
 						enemy.health -= 20
 						enemy.hitStun += 13
-			"elbow":
-				enemy.health -= 5
-			"kick":
-				enemy.health -= 5
+			
 			"punch":
-				enemy.health -= 5
+				block = "Standing"
+				if (abs(enemy.position.x - position.x) <= 48):
+					if block == "Standing":
+						enemy.health -= 7
+						enemy.hitStun += 6
+					else:
+						enemy.health -= 25
+						enemy.hitStun += 14
 	else:
 		match type:
 			"lowkick":
+				block = "Standing"
 				if (abs(enemy.position.x - position.x) <= 48):
 					print("Attacking Player " + str(enemy.playerNumber) + ".")
-					if p1block == "Down":
-						enemy.health -= 2
-						enemy.hitStun += 12
+					if block == "Standing":
+						enemy.health -= 4
+						enemy.hitStun += 11
 					else:
 						enemy.health -= 20
-						enemy.hitStun += 23
-			"elbow":
-				enemy.health -= 5
-			"kick":
-				enemy.health -= 5
+						enemy.hitStun += 13
+			
 			"punch":
-				enemy.health -= 5
+				block = "Standing"
+				if (abs(enemy.position.x - position.x) <= 100):
+					if block == "Standing":
+						enemy.health -= 7
+						enemy.hitStun += 6
+					else:
+						enemy.health -= 25
+						enemy.hitStun += 14
